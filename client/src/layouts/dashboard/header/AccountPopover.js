da99061f2d4884396b662668ff42aadc84d36117
leminhthanh9823 <thanhlmhe176868@fpt.edu.vn>
2025-02-26 17:50:26 +0700

    change method cal backend for logout

--- client/src/layouts/dashboard/header/AccountPopover.js
+++ client/src/layouts/dashboard/header/AccountPopover.js
@@ -1,9 +1,17 @@
-import { useState } from 'react';
-import { alpha } from '@mui/material/styles';
-import { Avatar, Box, Divider, IconButton, MenuItem, Popover, Typography } from '@mui/material';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
-import { useAuth } from '../../../hooks/useAuth';
+import { useState } from "react";
+import { alpha } from "@mui/material/styles";
+import {
+  Avatar,
+  Box,
+  Divider,
+  IconButton,
+  MenuItem,
+  Popover,
+  Typography,
+} from "@mui/material";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { useAuth } from "../../../hooks/useAuth";
 
 export default function AccountPopover() {
   const { user, logout } = useAuth(); // Destructure logout from useAuth()
@@ -12,7 +20,7 @@ export default function AccountPopover() {
   const logoutUser = () => {
     handleClose();
     axios
-      .get('http://localhost:8080/api/auth/logout', { withCredentials: true })
+      .post("http://localhost:8080/api/auth/logout", { withCredentials: true })
       .then((response) => {
         if (response.status === 200) {
           console.log(response.data);
@@ -33,7 +41,7 @@ export default function AccountPopover() {
     setOpen(null);
   };
 
-  console.log('User object:', user);
+  console.log("User object:", user);
 
   return (
     <>
@@ -42,54 +50,64 @@ export default function AccountPopover() {
         sx={{
           p: 0,
           ...(open && {
-            '&:before': {
+            "&:before": {
               zIndex: 1,
               content: "''",
-              width: '100%',
-              height: '100%',
-              borderRadius: '50%',
-              position: 'absolute',
+              width: "100%",
+              height: "100%",
+              borderRadius: "50%",
+              position: "absolute",
               bgcolor: (theme) => alpha(theme.palette.grey[900], 0.8),
             },
           }),
         }}
       >
-        <Avatar src={user?.photoUrl || '/default-photo-url.png'} alt={user?.name || 'User'} />
+        <Avatar
+          src={user?.photoUrl || "/default-photo-url.png"}
+          alt={user?.name || "User"}
+        />
       </IconButton>
 
       <Popover
         open={Boolean(open)}
         anchorEl={open}
         onClose={handleClose}
-        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+        anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+        transformOrigin={{ vertical: "top", horizontal: "right" }}
         PaperProps={{
           sx: {
             p: 0,
             mt: 1.5,
             ml: 0.75,
             width: 180,
-            '& .MuiMenuItem-root': {
-              typography: 'body2',
+            "& .MuiMenuItem-root": {
+              typography: "body2",
               borderRadius: 0.75,
             },
           },
         }}
       >
         <Box sx={{ my: 1.5, px: 2.5 }}>
-          <MenuItem style={{ paddingLeft: '3px' }}>
-            <Link to={`/userprofile/${user?._id}`} style={{ textDecoration: 'none' }}>
+          <MenuItem style={{ paddingLeft: "3px" }}>
+            <Link
+              to={`/userprofile/${user?._id}`}
+              style={{ textDecoration: "none" }}
+            >
               <Typography variant="subtitle2" noWrap>
                 {user?.name}
               </Typography>
-              <Typography variant="body2" sx={{ color: 'text.secondary' }} noWrap>
+              <Typography
+                variant="body2"
+                sx={{ color: "text.secondary" }}
+                noWrap
+              >
                 {user?.email}
               </Typography>
             </Link>
           </MenuItem>
         </Box>
 
-        <Divider sx={{ borderStyle: 'dashed' }} />
+        <Divider sx={{ borderStyle: "dashed" }} />
 
         <MenuItem onClick={logoutUser} sx={{ m: 1 }}>
           Logout
